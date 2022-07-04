79402f1abd1cae3cf21c389bf770738c4adfdc43
Marko Janjic <marko.m.janjic@gmail.com>
2022-07-04 11:14:02 +0200

    fixed user input vilidation

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 "use strict";
 
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
 // computer choice
 // function that handle computer choice
 // returns string value from the list
 function computerChoice() {
-  const choiceList = ["rock", "paper", "scissors"];
+  const choiceList = [rock, paper, paper];
 
   let i = Math.floor(Math.random() * 3);
   console.log(choiceList[i]);
@@ -14,15 +17,12 @@ function computerChoice() {
 //player choice
 //returns lowercase string value from the user prompt
 function playerChoice() {
-  let choice = prompt("Enter rock, paper, scissors: ");
+  let choice = prompt("Enter rock, paper, scissors: ").toLowerCase();
 
   //handle wrong user input
-
-  if (choice !== "rock" || choice !== "paper" || choice !== "scissors") {
+  if (choice !== rock && choice !== paper && choice !== scissors) {
     alert("Not valid choice");
-    choice = prompt("Enter valid choice");
-    console.log(choice);
-    return choice.toLocaleLowerCase();
+    choice = prompt("Enter valid choice").toLowerCase();
   }
   console.log(choice);
   return choice.toLowerCase();
@@ -34,21 +34,24 @@ function playerChoice() {
 function playRoud(player, computer) {
   let computerPlay = computer;
   let playerPlay = player;
+  const playerWonMessage = "player won";
+  const computerWonMessage = "computer won";
+  const tieGame = "It's tie game";
 
-  if (player === "rock" && computer === "scissors") {
-    console.log("player won");
-  } else if (player === "scissors" && computer === "rock") {
-    console.log("computer won");
-  } else if (player === "scissors" && computer === "paper") {
-    console.log("player won");
-  } else if (player === "paper" && computer === "scissors") {
-    console.log("computer won");
-  } else if (player === "paper" && computer === "rock") {
-    console.log("player won");
-  } else if (player === "rock" && computer === "paper") {
-    console.log("computer won");
+  if (player === rock && computer === scissors) {
+    console.log(playerWonMessage);
+  } else if (player === scissors && computer === rock) {
+    console.log(computerWonMessage);
+  } else if (player === scissors && computer === paper) {
+    console.log(playerWonMessage);
+  } else if (player === paper && computer === scissors) {
+    console.log(computerWonMessage);
+  } else if (player === paper && computer === rock) {
+    console.log(playerWonMessage);
+  } else if (player === rock && computer === paper) {
+    console.log(computerWonMessage);
   } else if (player === computer) {
-    console.log("tie game");
+    console.log(tieGame);
   }
 
   return computerPlay, playerPlay;
